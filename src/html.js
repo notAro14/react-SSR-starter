4836f82e317858223f6564e81422546ba316ef31
Aro Andriamaro <ni.aro.aina@gmail.com>
2021-02-07 14:18:43 +0100

    Manually minify html literals

--- src/html.js
+++ src/html.js
@@ -3,20 +3,7 @@ const HtmlString = ({
   title = '',
   scriptTags = '',
   styleTags = '',
-}) => `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charSet="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>${title}</title>
-    ${scriptTags}
-    ${styleTags}
-  </head>
-  <body>
-    <div id="root">${app}</div>
-  </body>
-</html>
-`;
+}) =>
+  `<!DOCTYPE html><html lang="en"><head><meta charSet="UTF-8" /><meta name="viewport" content="width=device-width, initial-scale=1.0" /><title>${title}</title>${scriptTags}${styleTags}</head><body><div id="root">${app}</div></body></html>`;
 
 export default HtmlString;
